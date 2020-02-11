1b3e90f32c63f64aa1166fef16daca9330a907b3
TomTom4 <pouvreau@etud.insa-toulouse.fr>
2020-02-11 13:53:43 +0100

    make legend work. there is a bug with style though

--- js/lib/legend.js
+++ js/lib/legend.js
@@ -3,68 +3,54 @@ var _ = require('lodash');
 var L = require('leaflet');
 const ipyleaflet = require('jupyter-leaflet');
 
-// See example.py for the kernel counterpart to this file.
+
+let jsLegend = L.control({position: "bottomright"})
+jsLegend.onAdd = function(map){
+	let jsLegendName="leaflet-control-legend"
+	let container = L.DomUtil.create('div', jsLegendName),
+	grades = [0, 10, 100],
+	labels = []
+
+	for (let i=0; i < grades.length; i++){
+		container.innerHTML += '<i style="background: \"blue\""></i> toto'
+	}
+	return container
+
+}
 
 
-// Custom Model. Custom widgets models must at least provide default values
-// for model attributes, including
-//
-//  - `_view_name`
-//  - `_view_module`
-//  - `_view_module_version`
-//
-//  - `_model_name`
-//  - `_model_module`
-//  - `_model_module_version`
-//
-//  when different from the base class.
-
-// When serialiazing the entire widget state for embedding, only values that
-// differ from the defaults will be specified.
-let LegendModel = new ipyleaflet.LeafletControlModel.extend({
-//var LegendModel = widgets.DOMWidgetModel.extend({
-    defaults: _.extend(ipyleaflet.LeafletControlModel.prototype.defaults(), {
-        _model_name : 'LegendModel',
-        _view_name : 'LegendView',
-        _model_module : 'jupyter-widget-legend',
-        _view_module : 'jupyter-widget-legend',
-        _model_module_version : '0.1.0',
-        _view_module_version : '0.1.0',
-        value : 'Legend World!'
-    })
-});
-
-
-// Custom View. Renders the widget model.
-let LegendView = ipyleaflet.LeafletControlView.extend({
-//var LegendView = widgets.DOMWidgetView.extend({
-    // Defines how the widget gets rendered into the DOM
-    render: function() {
-        this.value_changed();
-
-        // Observe changes in the value traitlet in Python, and define
-        // a custom callback.
-        this.model.on('change:value', this.value_changed, this);
-    },
-    value_changed: function() {
-		if(this.obj) this.obj.remove()
-		this.obj = L.control({position: 'bottomright'})
-		this.obj.onAdd = this.onAdd()
-		this.obj.addTo(this.map_view)
-    },
-	onAdd: function(map){
-		let div = L.DomUtil.create('div', 'info legend'),
-			grades = [0, 10, 100],
-			labels = []
-
-		for (let i=0; i < grades.length; i++){
-			div.innerHTML += '<i style="background: blue"></i> toto'
+export class LegendModel extends ipyleaflet.LeafletControlModel {
+	defaults(){
+		return{
+			...super.defaults(),
+			_model_name : 'LegendModel',
+        	_view_name : 'LegendView',
+        	_model_module : 'jupyter-widget-legend',
+        	_view_module : 'jupyter-widget-legend',
+        	_model_module_version : '0.1.0',
+        	_view_module_version : '0.1.0',
+			value : "hello :)"
 		}
 	}
-});
+}
 
 
-module.exports = {
-    LegendModel: LegendModel,
-    LegendView: LegendView
-};
+export class LegendView extends ipyleaflet.LeafletControlView{
+
+	initialize(parameters){
+		super.initialize(parameters);
+		this.map_view = this.options.map_view;
+	}
+
+	value_changed(){
+		console.log("value changed!")
+		if(this.obj) this.obj.remove()
+		this.obj = jsLegend
+		this.obj.addTo(this.map_view.obj)
+	}
+
+	render(){
+		this.value_changed();
+        this.model.on('change:value', this.value_changed, this);
+	}
+}
